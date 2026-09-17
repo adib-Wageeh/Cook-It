0b0bfb008db625b7cdce1b3e9224cfa0573abeb1
adib wageeh <adibwageeh1@icloud.com>
2026-09-17 11:28:37 +0300

    refactor: update MealItem layout for improved responsiveness and text handling

--- app/src/main/java/com/example/cookit/ui/MealItem.kt
+++ app/src/main/java/com/example/cookit/ui/MealItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,6 +18,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -34,10 +36,8 @@ fun MealItem(
     ) {
         Box(
             modifier = Modifier
-                .size(
-                    width = 174.dp,
-                    height = 214.dp
-                )
+                .height(214.dp)
+                .fillMaxWidth()
                 .padding(top = 64.dp)
                 .clip(RoundedCornerShape(size = 12.dp))
                 .background(LightGray)
@@ -64,6 +64,8 @@ fun MealItem(
                 meal.name,
                 fontSize = 16.sp,
                 textAlign = TextAlign.Center,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
                 fontWeight = FontWeight.SemiBold
             )
         }
